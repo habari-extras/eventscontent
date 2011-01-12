fdb241aeab14500c6de58a26e30de409ff641601
Konzertheld <USER@DOMAIN>
2011-01-12 03:59:59 +0000

    Added a lot of stuff to the eventsContent.
    - Create event posts that have location, time and date, and a tag for grouping multiple posts dealing with the same event
    - Search posts by location in the admin interface
    - Search posts by event in the admin interface
    - Output preformatted dates and times
    
    git-svn-id: http://svn.habariproject.org/habari-extras/plugins/eventscontent/trunk@3590 f4a90942-1786-4aa1-b092-57c31935ddc3

--- eventscontent.plugin.php
+++ eventscontent.plugin.php
@@ -1,14 +1,14 @@
 <?php
 
-class eventsContent extends Plugin
+class EventsContent extends Plugin
 { 
 	
 	/**
 	 * Create help file
-	 */
+	 **/
 	public function help() {
 		$str= '';
-		$str.= '<p>EventsContent adds the event content type to add an event.</p>';
+		$str.= '<p>EventsContent adds the event content type to add events that basically have a date and a location. Comes with a rewrite rule to display events by location.</p>';
 		$str.= '<h3>Installation Instructions</h3>';
 		$str.= '<p>Your theme needs to have a <code>event.single</code> template, or a generic <code>single</code> template. If it does not, you can usually copy <code>entry.single</code> to <code>event.single</code> and use it.</p>';
 		return $str;
@@ -64,34 +64,135 @@ class eventsContent extends Plugin
 	}
 	
 	/**
-	 * Modify publish form. We're going to add the custom 'address' field, as we like
-	 * to hold our events at addresses.
-	 */
+	 * Format the unix timestamp as readable date.
+	 **/
+	public static function format_date_out($value)
+	{
+		if(isset($value) && !empty($value))
+		{
+			// Get the user's preferred format or, if none set, the system's default format
+			$user_format = User::identify()->info->locale_date_format;
+			if(!isset($user_format) || empty($user_format)) $user_format = Options::get('dateformat');
+			
+			if(strstr($value, ";"))
+			{ // Output formatting for dates with start and end
+				list($start, $end) = explode(";", $value, 2);
+				$startformatter = HabariDateTime::date_create($start);
+				$endformatter = HabariDateTime::date_create($end);
+				(isset($user_format) && !empty($user_format)) ? $out = $startformatter->format($user_format) . " - " . $endformatter->format($user_format) : $out = $startformatter->format() . " - " . $endformatter->format();
+			}
+			else
+			{ // Output formatting for single dates
+				$formatter = HabariDateTime::date_create($value);
+				(isset($user_format) && !empty($user_format)) ? $out = $formatter->format($user_format) : $out = $formatter->format();
+			}
+			
+			return $out;
+		}
+		else return $value;
+	}
+	
+	/**
+	 * Format the unix timestamp as readable time.
+	 **/
+	public static function format_time_out($value)
+	{
+		if(isset($value) && !empty($value))
+		{
+			// Get the user's preferred format or, if none set, the system's default format
+			$user_format = User::identify()->info->locale_time_format;
+			if(!isset($user_format) || empty($user_format)) $user_format = Options::get('timeformat');
+			
+			// Format the time
+			$hdteventtime = HabariDateTime::date_create($value);
+			if(isset($user_format) && !empty($user_format))
+				$out = $hdteventtime->format($user_format);
+			else
+				$out = $hdteventtime->format();
+			return $out;
+		}
+		else return $value;
+	}
+	
+	/**
+	 * Modify publish form. We're going to add the custom 'eventdate' field, as we like
+	 * to hold our events at specific dates. We'll also add fields for the time, the location
+	 * and a field called "eventtag" which will help us group multiple posts for the same event.
+	 * We store the time separately to let the user decide in his theme what he wants to display.
+	 * Also, if we stored both together, the time would be 00:00 for events with no specific time.
+	 **/
 	public function action_form_publish($form, $post, $context)
 	{
-	  // only edit the form if it's an event
-		if ($form->content_type->value == Post::type('event')) {
-      // just want to add a text field
-      $form->insert('tags', 'text', 'address', 'null:null', _t('Event Address'), 'admincontrol_textArea');
-      $form->address->value = $post->info->address;
-      $form->address->template = 'admincontrol_text';
+		// only edit the form if it's an event
+		if ($form->content_type->value == Post::type('event'))
+		{
+			// add text fields
+			$form->insert('tags', 'text', 'eventtag', 'null:null', _t('Event Tag (for event grouping)'), 'admincontrol_textArea');
+			$form->insert('tags', 'text', 'location', 'null:null', _t('Event Location'), 'admincontrol_textArea');
+			$form->insert('tags', 'text', 'eventdate', 'null:null', _t('Event Date'), 'admincontrol_textArea');
+			$form->insert('tags', 'text', 'eventtime', 'null:null', _t('Event Time'), 'admincontrol_textArea');
+			// load values and display the fields
+			$form->eventtag->value = $post->info->eventtag;
+			$form->eventtag->template = 'admincontrol_text';
+			$form->location->value = $post->info->location;
+			$form->location->template = 'admincontrol_text';
+			// use the same function for displaying the date we use for displaying the date in the theme
+			$form->eventdate->value = EventsContent::format_date_out($post->info->eventdate);
+			$form->eventdate->template = 'admincontrol_text';
+			// the same for the time
+			$form->eventtime->value = EventsContent::format_time_out($post->info->eventtime);
+			$form->eventtime->template = 'admincontrol_text';
 		}
 	}
 	
 	/**
 	 * Save our data to the database
-	 */
+	 **/
 	public function action_publish_post( $post, $form )
 	{
-		if ($post->content_type == Post::type('event')) {
-			// Address exists because we made it in action_form_publish()
-			$post->info->address = $form->address->value;
+		if ($post->content_type == Post::type('event'))
+		{
+			$post->info->eventtag = $form->eventtag->value;
+			$post->info->location = $form->location->value;
+			// Save date and time as unix timestamp so we can order by date.
+			// Multiday events are supported by the following function, but first we do some formatting preparation
+			// This actually reverts what format_date_out() did before
+			$eventdate = str_replace("-", ";", $form->eventdate->value);
+			$eventdate = str_replace(" ", "", $eventdate);
+			$post->info->eventdate = EventsContent::eventdate_to_unix($eventdate);
+			// For the time, the reverting is simpler as multiple times are not yet supported
+			// If the field is empty, we remove the time from the database
+			// That's necessary because otherwise format_time_out would use the current time
+			$eventtime = $form->eventtime->value;
+			if(isset($eventtime) && !empty($eventtime))
+				$post->info->eventtime = HabariDateTime::date_create($eventtime)->int;
+			else
+				unset($post->info->eventtime);
 		}
+	}
+	
+	/**
+	 * Converts dates provided by the user to unix timestamps
+	 * This function is necessary for multiple dates
+	 **/
+	public static function eventdate_to_unix($eventdatestring)
+	{
+		$dates = explode(";", $eventdatestring, 2);
+		foreach($dates as $datestring)
+		{
+			try
+			{
+				$unixdates[] = HabariDateTime::date_create($datestring)->int;
+			} catch(Exception $crap) {return false;}
+		}
+		
+		if(count($unixdates)) return implode(";", $unixdates);
+		else return false;
 	}
 
 	/**
-	 * Add the 'events' type to the list of templates that we can use.
-	 */
+	 * Add the 'events' type to the list of templates that we can use. This is what makes Habari display events in the global post output.
+	 **/
 	public function filter_template_user_filters($filters) {
 		if(isset($filters['content_type'])) {
 			$filters['content_type']= Utils::single_array( $filters['content_type'] );
@@ -100,6 +201,105 @@ class eventsContent extends Plugin
 		return $filters;
 	}
 	
-}
+	/**
+	 * Modify output in the rss feed (include post info metadata)
+	 **/
+    public function action_rss_add_post( $feed_entry, $post )
+    {
+        $info = $post->info->get_url_args();
+        foreach( $info as $key => $value ) {
+            if( is_array( $value ) && isset( $value['enclosure'] ) ) {
+                $enclosure = $feed_entry->addChild( 'enclosure' );
+                $enclosure->addAttribute( 'url', $value['enclosure'] );
+                $enclosure->addAttribute( 'length', $value['size'] );
+                $enclosure->addAttribute( 'type', 'text' );
+            }
+        }
+    }
 
+	/**
+	 * Modify output in the atom feed (include post info metadata)
+	 **/
+    public function action_atom_add_post( $feed_entry, $post )
+    {
+//        $info = $post->info->get_url_args();
+//        foreach( $info as $key => $value ) {
+//            if( is_array( $value ) && isset( $value['enclosure'] ) ) {
+//                $enclosure = $feed_entry->addChild( 'link' );
+//                $enclosure->addAttribute( 'rel', 'enclosure' );
+//                $enclosure->addAttribute( 'href', $value['enclosure'] );
+//                $enclosure->addAttribute( 'length', $value['size'] );
+//                $enclosure->addAttribute( 'type', 'text' );
+//            }
+//        }
+		if(Post::type("event")==$post->content_type)
+			$feed_entry->content[0] = "<strong>Event @ ".$post->info->location.", ".$post->info->eventdate_out.":</strong> ".$feed_entry->content[0];
+    }
+	
+	/**
+	 * Add events to the atom feed
+	 **/
+	public function filter_atom_get_collection_content_type( $content_type )
+    {
+        $content_type = Utils::single_array( $content_type );
+        $content_type[] = Post::type( 'event' );
+        return $content_type;
+    }
+	
+	/**
+	 * Make posts searchable by locations and eventtags on the admin manage page
+	 **/
+    public function filter_posts_search_to_get ( $arguments, $flag, $value, $match, $search_string)
+    {
+        if($flag == 'location') {
+            $arguments['info'] = array('location'=>$value);
+        }
+		else if($flag == 'eventtag') {
+			$arguments['info'] = array('eventtag'=>$value);
+		}
+        return $arguments;
+    }
+    
+	/**
+	 * Add rewrite rules to handle locations and eventtags
+	 **/
+    public function filter_rewrite_rules($rules)
+    {
+        $rules[] = RewriteRule::create_url_rule('"location"/location_name', 'PluginHandler', 'location');
+		$rules[] = RewriteRule::create_url_rule('"eventtag"/eventtag_value', 'PluginHandler', 'eventtag');
+        return $rules;
+    }
+    
+    /**
+	 * Implement the output for posts by location
+	 **/
+    public function action_plugin_act_location($handler)
+    {
+        $handler->theme->act_display_entries(array('info' => array('location' => $handler->handler_vars['location_name'])));
+    }
+	
+	 /**
+	 * Implement the output for posts by eventtag
+	 **/
+    public function action_plugin_act_eventtag($handler)
+    {
+        $handler->theme->act_display_entries(array('info'=>array('eventtag'=>$handler->handler_vars['eventtag_value'])));
+    }
+	
+	/**
+	 * Convert the date to the user's preferred date format when it is requested by the theme with eventdate_out.
+	 **/
+	public function filter_post_info_eventdate_out($value)
+	{
+		return EventsContent::format_date_out($value);
+	}
+	
+	/**
+	 * Convert the time to the user's preferred time format when it is requested by the theme with eventtime_out.
+	 **/
+	public function filter_post_info_eventtime_out($value)
+	{
+		return EventsContent::format_time_out($value);
+	}
+}
 ?>
